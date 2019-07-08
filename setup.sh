f17c60e91cccaf68c06db0e686bcef31157fb4ba
Zack Lofgren <zack@zlofgren.com>
2019-07-08 13:10:10 -0600

    cleaned up script and added newsboat and bin dirs

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-# setup.sh
-# Zack Lofgren
-# 17 May 2019
 dir=$(pwd)
 
 # create symlinks for each file
 ln -s $dir/irssi ~/.irssi
 ln -s $dir/tmux.conf ~/.tmux.conf
-ln -s $dir/xsession ~/.xsession
 ln -s $dir/xinitrc ~/.xinitrc
 ln -s $dir/zshrc ~/.zshrc
 
@@ -15,4 +11,10 @@ if [ ! -d ~/.config ]; then
 	mkdir ~/.config
 fi
 
+if [ ! -d ~/.local ]; then
+	mkdir ~/.local
+fi
+
 ln -s $dir/config/nvim ~/.config/nvim
+ln -s $dir/config/newsboat ~/.config/newsboat
+ln -s $dir/local ~/.local
